1528815590f22220109fed487e0f40dd1f0fb796
Nikola Borisic <borisic.nikola@yahoo.com>
2017-11-08 17:21:11 +0100

    add menu submit

--- source/js/views/Admin/index.jsx
+++ source/js/views/Admin/index.jsx
@@ -20,13 +20,15 @@ export default class Admin extends Component {
     this.state = {
       fromDate: null,
       toDate: null,
+      formSubmit: null,
+      formError: null,
       menu: '',
     };
 
     this.postToFirebase = this.postToFirebase.bind(this);
     this.onInputChange = this.onInputChange.bind(this);
-    this.setMinDate = this.setMinDate.bind(this);
-    this.setMaxDate = this.setMaxDate.bind(this);
+    this.setFromDate = this.setFromDate.bind(this);
+    this.setToDate = this.setToDate.bind(this);
   }
   onInputChange(e) {
     this.setState({
@@ -35,45 +37,97 @@ export default class Admin extends Component {
   }
 
   setFromDate(e, date) {
+    const reDate = new Intl.DateTimeFormat('en-GB').format(date).replace(/\//g, '-');
     this.setState({
-      fromDate: date,
+      fromDate: reDate,
     });
   }
 
   setToDate(e, date) {
+    const reDate = new Intl.DateTimeFormat('en-GB').format(date).replace(/\//g, '-');
     this.setState({
-      toDate: date,
+      toDate: reDate,
     });
   }
 
   postToFirebase(e) {
     e.preventDefault();
 
+    const stringArray = this.state.menu.split(/\n[IVabc]{1,4}\n|\n{3}/);
+    let day;
+    const weekMenuArray = [];
+    for (let i = 0; i < stringArray.length; i++) {
+      for (let j = 0; j < weekDays.length; j++) {
+        if (stringArray[i].includes(weekDays[j])) {
+          day = j;
+          weekMenuArray[day] = [];
+          stringArray[i] = stringArray[i].replace(/[\d,.\s]/g, '');
+          break;
+        }
+      }
+      weekMenuArray[day].push(stringArray[i].trim());
+    }
+
+    const reducedObj = function (acc, currentValue) {
+      return Object.assign(acc, {
+        [currentValue[0]]: (function () {
+          let dayObj = {};
+          for (let i = 1; i < currentValue.length; i++) {
+            const oneDish = { [i]: currentValue[i] };
+            dayObj = Object.assign(dayObj, oneDish);
+          }
+          return dayObj;
+        }()),
+      });
+    };
+
+    const weekMeny = weekMenuArray.reduce(reducedObj, {});
+    // console.log(weekMenuArray);
+
+    const menuObject = {};
+    menuObject[`from ${ this.state.fromDate } to ${ this.state.toDate }`] = weekMeny;
+    console.log(menuObject);
+
     if (firebaseCurrentUser()) {
       firebaseCurrentUser().getIdToken(true).then((idToken) => {
         fetch(`https://yamzor-2.firebaseio.com/menu.json?auth=${ idToken }`, {
-          method: 'PUT',
+          method: 'PATCH',
           headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json',
           },
-          body: JSON.stringify(this.state.menu),
+          body: JSON.stringify(menuObject),
         });
-      }).then(console.log(this.state.menu));
+      })
+      .then(
+        () => {
+          this.setState({
+            formSubmit: 'Forma uspesno poslata',
+          });
+        }
+      )
+      .catch(
+        () => {
+          this.setState({
+            formError: 'Ups, nesto je poslo po zlu',
+          });
+        }
+      );
     }
   }
 
   render() {
-    console.log(this.state);
     return (
       <div className='Admin'>
         hello admineee
         <DatePicker
+          formatDate={ (date) => new Intl.DateTimeFormat('en-GB').format(date) }
           autoOk={ true }
           onChange={ this.setFromDate }
           hintText='od'
         />
         <DatePicker
+          formatDate={ (date) => new Intl.DateTimeFormat('en-GB').format(date) }
           autoOk={ true }
           onChange={ this.setToDate }
           hintText='do'
@@ -86,6 +140,7 @@ export default class Admin extends Component {
             value={ this.state.menu }
           />
           <button type='submit'> Posalji </button>
+          <span>{ this.state.formError | this.state.formSubmit  }</span>
         </form>
       </div>
     );
